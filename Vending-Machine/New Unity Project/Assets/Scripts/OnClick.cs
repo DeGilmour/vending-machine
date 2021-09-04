2e5fa026df5b2844e2f4dda9da56702976e72443
DeGilmour <alvaotuca@gmail.com>
2021-09-04 13:18:47 -0300

    Added basic logic to the scripts

--- Vending-Machine/New Unity Project/Assets/Scripts/OnClick.cs
+++ Vending-Machine/New Unity Project/Assets/Scripts/OnClick.cs
@@ -10,11 +10,12 @@ public class OnClick : MonoBehaviour
     // Nao consigo acessar o script sem ser pegando do objeto, se voces conseguirem arrumem isso.
     public GameObject vendingMachine;
     public VendingMachine vendingMachineObj;
+    public double money;
     void Start()
     {
-        doceA.onClick.AddListener(() => acceptCandy(1));
-        doceB.onClick.AddListener(() => acceptCandy(2));
-        doceC.onClick.AddListener(() => acceptCandy(3));
+        doceA.onClick.AddListener(() => acceptCandy(1, money));
+        doceB.onClick.AddListener(() => acceptCandy(2, money));
+        doceC.onClick.AddListener(() => acceptCandy(3, money));
         vendingMachineObj = vendingMachine.GetComponent<VendingMachine>();
     }
 
@@ -24,8 +25,8 @@ public class OnClick : MonoBehaviour
 
     }
 
-    public void acceptCandy(int type){
-        vendingMachineObj.acceptCandy(type);
+    public void acceptCandy(int type, double money){
+        vendingMachineObj.acceptCandy(type, money);
     }
 
 }   
--- Vending-Machine/New Unity Project/Assets/Scripts/VendingMachine.cs
+++ Vending-Machine/New Unity Project/Assets/Scripts/VendingMachine.cs
@@ -8,9 +8,13 @@ public class VendingMachine : MonoBehaviour
     // Start is called before the first frame update
     private int credit;
     private int[] candiesInMachine = new int[15];
+    private Dictionary<int, int> candy_value = new Dictionary<int, int>();
+ 
     void Start()
     {
-        
+        candy_value.Add(1, 6);
+        candy_value.Add(2, 7);
+        candy_value.Add(3, 8);
     }
 
     // Update is called once per frame
@@ -19,8 +23,15 @@ public class VendingMachine : MonoBehaviour
         
     }
 
-    public void acceptCandy(int candy_type){
-        Debug.Log("Candy Type " + candy_type);
+    public void acceptCandy(int candy_type, double money){
+        var diff = money.CompareTo(candy_type);
+        double change = Math.Abs(money -  candy_value[candy_type]);
+        if(diff > 0){
+            Debug.Log("Give candy and change back, change: " + change);
+        }
+        else{
+            Debug.Log("Not enough money");
+        }
     }
 
 }
