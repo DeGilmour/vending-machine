bfb5eb1dc50104824df4ecb2dba3ac3f864bf138
DeGilmour <alvaotuca@gmail.com>
2021-09-13 21:33:52 -0300

    Created the onclick sprites

--- Vending-Machine/New Unity Project/Assets/Scripts/OnClick.cs
+++ Vending-Machine/New Unity Project/Assets/Scripts/OnClick.cs
@@ -6,16 +6,20 @@ using UnityEngine.UI;
 
 public class OnClick : MonoBehaviour
 {
-    public Button doceA, doceB, doceC;
+    // public Button doceA, doceB, doceC;
+    public GameObject doceA, doceB, doceC;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite, oldSprite;
     // Nao consigo acessar o script sem ser pegando do objeto, se voces conseguirem arrumem isso.
     public GameObject vendingMachine;
     public VendingMachine vendingMachineObj;
     void Start()
     {
-        doceA.onClick.AddListener(() => acceptCandy(1));
-        doceB.onClick.AddListener(() => acceptCandy(2));
-        doceC.onClick.AddListener(() => acceptCandy(3));
+        // doceA.onClick.AddListener(() => acceptCandy(1));
+        // doceB.onClick.AddListener(() => acceptCandy(2));
+        // doceC.onClick.AddListener(() => acceptCandy(3));
         vendingMachineObj = vendingMachine.GetComponent<VendingMachine>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,4 +32,31 @@ public class OnClick : MonoBehaviour
         vendingMachineObj.acceptCandy(type);
     }
 
+    void OnMouseDown()
+    {
+        
+        ChangeSprite(1);
+        int type = 0;
+        if(this.gameObject.name == "buttonA"){
+            type = 1;
+        }
+        else if(this.gameObject.name == "buttonB")
+            type = 2;
+        else
+            type = 3;
+        acceptCandy(type);
+    }
+    
+    void OnMouseUp()
+    {
+        ChangeSprite(0);
+        
+    }
+    void ChangeSprite(int type) 
+    {
+        if(type == 1)
+            spriteRenderer.sprite = newSprite;
+        else
+            spriteRenderer.sprite = oldSprite;
+    }
 }   
